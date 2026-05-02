4e82551c66e3271d9bdfc302db35efff82ec27d8
Nomi Wang <zhanghewang7@gmail.com>
2026-05-01 23:39:45 -0500

    changed this code to be able to handle two inputs.

--- numbers.c
+++ numbers.c
@@ -3,11 +3,17 @@
 
 int main()
 {
-  int x;
+  int x, y, answer;
   
- printf("Choose a number\n");
+ printf("Choose a number:\n");
 
  scanf("%d", &x);
 
- printf("Number chose is %d\n", x);
+ printf("Select another number:\n");
+
+ scanf("%d", &y);
+
+ answer = x + y;
+
+ printf("The sum of these two numbers, %d and %d, is  %d\n", x, y, answer);
 }
